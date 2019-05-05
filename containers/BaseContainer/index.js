a96388cd651bcf499fcb97ee6cb5504fc27dcfef
indaam <indaam@gmail.com>
2019-05-06 02:29:18 +0700

    Update Basecontainer

--- containers/BaseContainer/index.js
+++ containers/BaseContainer/index.js
@@ -1,7 +1,43 @@
 import * as React from 'react';
+import { COLORS, METRICS } from "../../config";
+import { HELPERS } from "../../utils";
+import { View, Text } from 'react-native';
 
 export default class BaseContainer extends React.Component {
     state = {
         name: "BaseContainer"
     }
+
+    navGoBack(goBackParams){
+        console.log("goBack FROM BASE CONTAINER")
+        const { navigation } = this.props;
+        goBackParams ? navigation.goBack(goBackParams) : navigation.goBack()
+    }
+
+    navNavigate(navigateParams) {
+        console.log("navigateTo FROM BASE CONTAINER")
+        const { navigation } = this.props;
+        navigation.navigate(navigateParams);
+    }
+
+    renderHeader(data) {
+        const { title } = data;
+        console.log("renderHeader FROM BASE CONTAINER");
+        const wrapper = {
+            margin: METRICS.gutter.large,
+        }
+        const heading = {
+            color: COLORS.softBlack,
+            fontSize: 30,
+            textAlign: "center"
+        }
+        const subHeading = {
+            ...heading,
+            fontSize: 18,
+        }
+        return (<View style={{ ...wrapper }}>
+            <Text style={{ ...subHeading }}>{title}</Text>
+        </View>)
+    }
+
 }
